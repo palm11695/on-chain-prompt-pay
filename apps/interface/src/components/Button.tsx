ff78d545f4f93b9731d3bd199ca86b44b2c7bb41
johnwangxyz <johnwang.xlp@gmail.com>
2023-10-28 14:44:33 +0700

    feat: home

--- apps/interface/src/components/Button.tsx
+++ apps/interface/src/components/Button.tsx
@@ -1,6 +1,19 @@
 import { PropsWithChildren } from 'react'
 
-const Button = ({ children }: PropsWithChildren) => {
+type Props = PropsWithChildren<{
+  onClick?: () => void
+  variant?: 'primary' | 'secondary'
+}>
+
+const Button = ({ children, variant }: Props) => {
+  if (variant === 'secondary') {
+    return (
+      <button className="w-full rounded-xl bg-blue-200 py-3 font-semibold text-blue-600">
+        {children}
+      </button>
+    )
+  }
+
   return (
     <button className="w-full rounded-xl bg-blue-600 py-3 font-semibold text-white">
       {children}
--- apps/interface/src/pages/Home/Home.tsx
+++ apps/interface/src/pages/Home/Home.tsx
@@ -3,6 +3,7 @@
 // import { ConnectButton } from '@rainbow-me/rainbowkit'
 
 import Button from '../../components/Button'
+import Container from '../../components/Container'
 
 export const Home = () => {
   // const [count, setCount] = useState(0)
@@ -10,13 +11,38 @@ export const Home = () => {
   // console.log('account:', account)
   // return <div className="h-full">Home page</div>
   return (
-    <div className="container mx-auto">
-      <div className="h-screen px-4 py-6">
-        <div className="flex h-full flex-col items-center justify-center gap-y-4">
-          <div className="text-xl font-semibold">OnChainPromptPay</div>
-          <Button>Connect Wallet</Button>
+    <Container>
+      <div className="w-full rounded-xl bg-blue-600 p-4 text-white">
+        <div className="text-sm text-blue-200">Usable balance</div>
+        <div className="text-2xl font-medium">฿10,000.00</div>
+
+        <div className="h-8" />
+
+        <div className="text-xs font-light text-blue-300">
+          Spending wallet address
+        </div>
+        <div className="text-xs text-blue-200">0xc0ff...4979</div>
+      </div>
+
+      <div className="h-8" />
+
+      <div className="text-lg font-semibold">My Assets</div>
+      <div className="h-4" />
+      <div className="flex items-center justify-between">
+        <div>
+          <div>ETH</div>
+          <div className="text-sm text-slate-400">Ethereum</div>
+        </div>
+        <div className="text-right">
+          <div>1 ETH</div>
+          <div className="text-sm text-slate-400">63,000.00฿</div>
         </div>
       </div>
-    </div>
+
+      <div className="fixed bottom-0 left-0 flex w-full flex-col gap-y-1.5 px-4 pb-4">
+        <Button>Pay via QR</Button>
+        <Button variant="secondary">Topup</Button>
+      </div>
+    </Container>
   )
 }
