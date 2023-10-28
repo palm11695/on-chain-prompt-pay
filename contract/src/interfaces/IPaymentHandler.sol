9b1173926e2645bd03ce210ffd41bd0545904702
palm11695 <natanonritta@gmail.com>
2023-10-28 18:40:04 +0700

    feat: init transfer req

--- contract/src/interfaces/IPaymentHandler.sol
+++ contract/src/interfaces/IPaymentHandler.sol
@@ -4,6 +4,9 @@ pragma solidity 0.8.21;
 interface IPaymentHandler {
   error PaymentHandler_AmountIsZero();
   error PaymentHandler_InsufficientBalance();
+  error PaymentHandler_SignerIsNotOperator();
+  error PaymentHandler_ExceedDeadline();
+  error PaymentHandler_Unauthorized();
 
   function fund(address _aaSigner, uint256 _amount) external;
 
@@ -11,7 +14,15 @@ interface IPaymentHandler {
 
   function withdrawAll(address _recipient) external;
 
-  function initTransferRequest() external;
+  // check sign message is from operator
+  function initTransferRequest(
+    uint256 _thbAmount,
+    uint256 _exchangeRate,
+    uint256 _deadline,
+    uint8 _v,
+    bytes32 _r,
+    bytes32 _s
+  ) external;
 
   function cancelTransferRequest() external;
 
