78de46761b891c966bdd61f67e991b1cea3c4e87
johnwangxyz <johnwang.xlp@gmail.com>
2023-10-28 14:14:02 +0700

    feat: create spending wallet

---
+++ apps/interface/src/pages/CreateSpendingWallet/CreateSpendingWallet.tsx
@@ -0,0 +1,18 @@
+const CreateSpendingWallet = () => {
+  return (
+    <div className="container mx-auto px-4 py-6">
+      <div className="text-lg font-semibold">Create Spending Wallet</div>
+
+      <div className="h-8" />
+
+      <div>Enter pin</div>
+      <div>Pin input here</div>
+
+      <div className="h-12" />
+
+      <button className="w-full rounded-lg bg-blue-600 py-3 font-semibold text-white">Continue</button>
+    </div>
+  )
+}
+
+export default CreateSpendingWallet
--- apps/interface/src/router.tsx
+++ apps/interface/src/router.tsx
@@ -1,16 +1,21 @@
-import { createBrowserRouter } from "react-router-dom";
-import { Home } from "./pages/Home/Home";
-import { Login } from "./pages/Login";
+import { createBrowserRouter } from 'react-router-dom'
+import { Home } from './pages/Home/Home'
+import { Login } from './pages/Login'
+import CreateSpendingWallet from './pages/CreateSpendingWallet/CreateSpendingWallet'
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <Home />,
   },
   {
-    path: "/login",
+    path: '/login',
     element: <Login />,
   },
-]);
+  {
+    path: '/create-spending-wallet',
+    element: <CreateSpendingWallet />,
+  },
+])
 
-export default router;
+export default router
